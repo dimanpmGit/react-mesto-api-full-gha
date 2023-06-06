3da5509446a141d8287a33ba0f8d0aa68ea9755e
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-06-06 23:32:03 +0700

    Sprint 15. Env done.

--- backend/app.js
+++ backend/app.js
@@ -57,6 +57,7 @@ app.use(errors()); // обработчик ошибок celebrate
 // Мидлвар централизованной обработки ошибок
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
+  console.log(err);
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
