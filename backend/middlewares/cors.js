82b92dba90b9625bcde1de8b988320279250272b
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-06-08 11:36:24 +0700

    Sprint 15. Review1 done.

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,8 +5,7 @@ const allowedCors = [
   'https://mesto.dimanpm.nomoredomains.rocks',
   'http://mesto.dimanpm.nomoredomains.rocks',
   'https://api.mesto.dimanpm.nomoredomains.rocks',
-  'http://api.mesto.dimanpm.nomoredomains.rocks',
-  'http://localhost:3000'
+  'http://api.mesto.dimanpm.nomoredomains.rocks'
 ];
 
 // eslint-disable-next-line consistent-return
